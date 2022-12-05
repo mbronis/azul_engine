d206c4824e329dace5cebb1ef827ecefe8ef55cf
mbronis <michal.bronis@gmail.com>
2022-12-06 00:47:40 +0100

    Board cleanup

--- src/env/board/board.py
+++ src/env/board/board.py
@@ -1,7 +1,7 @@
 """Represents player's board"""
 from typing import Optional, List
 
-from src.env.tiles import Tile
+from src.env.tiles import Tile, SingleTileLine
 from src.env.board.wall import Wall
 from src.env.board.pattern_lines import PatternLines
 from src.env.board.floor_line import FloorLine
@@ -14,9 +14,25 @@ class Board:
         self.pattern_lines = PatternLines(self.get_size())
         self.floor_line = FloorLine()
 
+    # ------------------
+    # -- Info
+    # ------------------
     def get_size(self) -> int:
         return self.wall.size
 
+    # ------------------
+    # -- Actions
+    # ------------------
+    def can_add(self, row: int, l: SingleTileLine) -> bool:
+        if not self.pattern_lines.can_add(row, l):
+            return False
+        if not self.wall.can_add_to_row(row, l.tile):
+            return False
+        return True
+
+    # ------------------
+    # -- Mechanics
+    # ------------------
     def add_score(self, value: int) -> None:
         self.score = max(0, self.score + value)
 
