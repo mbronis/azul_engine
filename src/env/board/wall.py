e5b8959bed65fd8ae028097decd82f6bf42a0bbf
mbronis <michal.bronis@gmail.com>
2022-12-05 08:16:39 +0100

    Added Wall.count_neighbors

--- src/env/board/wall.py
+++ src/env/board/wall.py
@@ -3,12 +3,15 @@ from typing import List, Optional
 from src.env.tiles import Tile
 
 
+WallTiles = List[List[Optional[Tile]]]
+
+
 class Wall:
     SIZE = 5
 
-    def __init__(self) -> None:
-        self._filled: List[List[Optional[Tile]]] = [[None for _ in range(self.SIZE)] for _ in range(self.SIZE)]
-        self._expected: List[List[Optional[Tile]]] = [[None for _ in range(self.SIZE)] for _ in range(self.SIZE)]
+    def __init__(self, filled: WallTiles = None, expected: WallTiles = None) -> None:
+        self._filled = filled or [[None for _ in range(self.SIZE)] for _ in range(self.SIZE)]
+        self._expected = expected or [[None for _ in range(self.SIZE)] for _ in range(self.SIZE)]
 
     def is_filled(self, row: int, col: int) -> bool:
         return self._filled[row][col] is not None
@@ -21,6 +24,34 @@ class Wall:
 
     def get_col_fills(self, col: int) -> set[Tile]:
         return set(self._filled[r][col] for r in range(self.SIZE) if self._filled[r][col])
+
+    def count_neighbors_row(self, row: int, col: int) -> int:
+        neighbors = 0
+        for i in range(self.SIZE - 1 - col):
+            if not self.is_filled(row, col + i + 1):
+                break
+            neighbors += 1
+        for i in range(col):
+            if not self.is_filled(row, col - i - 1):
+                break
+            neighbors += 1
+        return neighbors
+
+    def count_neighbors_col(self, row: int, col: int) -> int:
+        neighbors = 0
+        for i in range(self.SIZE - 1 - row):
+            print(f"{row + i + 1, col}: {self.is_filled(row + i + 1, col)}")
+            if not self.is_filled(row + i + 1, col):
+                break
+            neighbors += 1
+        for i in range(row):
+            if not self.is_filled(row - i - 1, col):
+                break
+            neighbors += 1
+        return neighbors
+
+    def count_neighbors(self, row: int, col: int) -> int:
+        return self.count_neighbors_row(row, col) + self.count_neighbors_col(row, col)
 
 
 class FixedWall(Wall):
---
+++ tests/wall/test_neighbor.py
@@ -0,0 +1,81 @@
+import pytest
+
+from src.env.board.wall import Wall
+
+
+@pytest.fixture
+def wall_a() -> Wall:
+    return Wall(
+        filled=[
+            [True, True, None, None, True],
+            [True, None, None, None, None],
+            [None, None, None, True, None],
+            [None, None, True, None, True],
+            [True, None, None, True, None],
+        ]
+    )
+
+
+scenarios = [
+    {"name": "wall_a, 0, 0", "row": 0, "col": 0, "expected": (1, 1, 2)},
+    {"name": "wall_a, 1, 0", "row": 1, "col": 0, "expected": (0, 1, 1)},
+    {"name": "wall_a, 0, 1", "row": 0, "col": 1, "expected": (1, 0, 1)},
+    {"name": "wall_a, 1, 1", "row": 1, "col": 1, "expected": (1, 1, 2)},
+    {"name": "wall_a, 2, 0", "row": 2, "col": 0, "expected": (0, 2, 2)},
+    {"name": "wall_a, 0, 2", "row": 0, "col": 2, "expected": (2, 0, 2)},
+    {"name": "wall_a, 2, 1", "row": 2, "col": 1, "expected": (0, 0, 0)},
+    {"name": "wall_a, 3, 0", "row": 3, "col": 0, "expected": (0, 1, 1)},
+    {"name": "wall_a, 0, 3", "row": 0, "col": 3, "expected": (1, 0, 1)},
+    {"name": "wall_a, 3, 2", "row": 3, "col": 2, "expected": (0, 0, 0)},
+    {"name": "wall_a, 2, 3", "row": 2, "col": 3, "expected": (0, 0, 0)},
+    {"name": "wall_a, 3, 3", "row": 3, "col": 3, "expected": (2, 2, 4)},
+]
+
+
+@pytest.mark.parametrize("scen", scenarios, ids=(scen["name"] for scen in scenarios))
+def test_neighbor_wall_a(scen, wall_a: Wall):
+    act_row = wall_a.count_neighbors_row(scen["row"], scen["col"])
+    act_col = wall_a.count_neighbors_col(scen["row"], scen["col"])
+    act_total = wall_a.count_neighbors(scen["row"], scen["col"])
+    exp_row, exp_col, exp_total = scen["expected"]
+    assert act_row == exp_row
+    assert act_col == exp_col
+    assert act_total == exp_total
+
+
+@pytest.fixture
+def wall_b() -> Wall:
+    return Wall(
+        filled=[
+            [True, None, True, True, None],
+            [None, None, None, None, None],
+            [True, None, None, None, None],
+            [True, None, None, None, None],
+            [None, None, None, None, None],
+        ]
+    )
+
+
+scenarios = [
+    {"name": "wall_b, 0, 0", "row": 0, "col": 0, "expected": (0, 0, 0)},
+    {"name": "wall_b, 0, 1", "row": 0, "col": 1, "expected": (3, 0, 3)},
+    {"name": "wall_b, 0, 2", "row": 0, "col": 2, "expected": (1, 0, 1)},
+    {"name": "wall_b, 0, 3", "row": 0, "col": 3, "expected": (1, 0, 1)},
+    {"name": "wall_b, 0, 4", "row": 0, "col": 4, "expected": (2, 0, 2)},
+    {"name": "wall_b, 0, 0", "row": 0, "col": 0, "expected": (0, 0, 0)},
+    {"name": "wall_b, 1, 0", "row": 1, "col": 0, "expected": (0, 3, 3)},
+    {"name": "wall_b, 2, 0", "row": 2, "col": 0, "expected": (0, 1, 1)},
+    {"name": "wall_b, 3, 0", "row": 3, "col": 0, "expected": (0, 1, 1)},
+    {"name": "wall_b, 4, 0", "row": 4, "col": 0, "expected": (0, 2, 2)},
+]
+
+
+@pytest.mark.parametrize("scen", scenarios, ids=(scen["name"] for scen in scenarios))
+def test_neighbor_wall_b(scen, wall_b: Wall):
+    act_row = wall_b.count_neighbors_row(scen["row"], scen["col"])
+    act_col = wall_b.count_neighbors_col(scen["row"], scen["col"])
+    act_total = wall_b.count_neighbors(scen["row"], scen["col"])
+    exp_row, exp_col, exp_total = scen["expected"]
+    assert act_row == exp_row
+    assert act_col == exp_col
+    assert act_total == exp_total
