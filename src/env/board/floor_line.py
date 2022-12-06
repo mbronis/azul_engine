f126ab1de88ad9b92219cbc62dc1e60e7e3768ed
mbronis <michal.bronis@gmail.com>
2022-12-07 00:48:18 +0100

    Rename attributes of FloorLine

--- src/env/board/floor_line.py
+++ src/env/board/floor_line.py
@@ -1,19 +1,20 @@
 """Represents floor line"""
 
+SIZE = 7
+
 
 class FloorLine:
-    def __init__(self, size: int = 7) -> None:
-        self.size = size
-        self.filled: int = 0
+    def __init__(self) -> None:
+        self.broken_tiles_count: int = 0
         self.has_first_player_token: bool = False
 
-    def add_tiles(self, value: int) -> None:
-        self.filled += value
-        self.filled = min(self.size, self.filled)
+    def add_broken_tiles(self, value: int) -> None:
+        self.broken_tiles_count += value
+        self.broken_tiles_count = min(SIZE, self.broken_tiles_count)
 
     def add_first_player_token(self) -> None:
         self.has_first_player_token = True
 
     def reset(self) -> None:
-        self.filled = 0
+        self.broken_tiles_count = 0
         self.has_first_player_token = False
