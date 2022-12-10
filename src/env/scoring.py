af76920b833060bd875877c3c1fc9225b983293c
mbronis <michal.bronis@gmail.com>
2022-12-10 15:23:45 +0100

    Refactor Scorer to be initialized with AzulRules

--- src/env/scoring.py
+++ src/env/scoring.py
@@ -1,47 +1,42 @@
+from src.env.board.rules import AzulRules
 from src.env.actions import AddWallTileAction
 from src.env.states.wall_state import WallState
 
 
 class Scorer:
-    # TODO: make abc and refactor to abstract attributes
-    BASE_NEIGHBOR_SCORE = 1
-    FILLED_ROW_SCORE = 2
-    FILLED_COL_SCORE = 7
-    FILLED_COLOR_SCORE = 10
-    FLOOR_PENALTIES = [-1, -1, -2, -2, -2, -3, -3]
+    def __init__(self, rules: AzulRules):
+        self.base_wall_fill_points = rules.points_self
+        self.filled_row_points = rules.points_row_fill
+        self.filled_col_points = rules.points_col_fill
+        self.filled_color_points = rules.points_color_fill
+        self.floor_penalties = rules.floor_penalties
 
-    @classmethod
-    def score_add_wall_tile(cls, s: WallState, a: AddWallTileAction) -> int:
+    def score_add_wall_tile(self, s: WallState, a: AddWallTileAction) -> int:
         neighbors_row = s.count_neighbors_row(a.row, a.col)
         neighbors_col = s.count_neighbors_col(a.row, a.col)
         tile_count = s.get_tile_count(a.tile)
 
         score = 0
-        score += cls._score_num_neighbor(neighbors_row, neighbors_col)
-        score += cls._score_filled_row(neighbors_row, s.size)
-        score += cls._score_filled_col(neighbors_row, s.size)
-        score += cls._score_filled_color(tile_count, s.size)
+        score += self._score_num_neighbor(neighbors_row, neighbors_col)
+        score += self._score_filled_row(neighbors_row, s.size)
+        score += self._score_filled_col(neighbors_row, s.size)
+        score += self._score_filled_color(tile_count, s.size)
         return score
 
-    @classmethod
-    def score_floor_line_flush(cls, floor_len: int) -> int:
-        return sum(cls.FLOOR_PENALTIES[:floor_len])
+    def score_floor_line_penalty(self, floor_len: int) -> int:
+        return sum(self.floor_penalties[:floor_len])
 
-    @classmethod
-    def _score_num_neighbor(cls, neighbors_row: int, neighbors_col: int) -> int:
-        return cls.BASE_NEIGHBOR_SCORE + neighbors_row + neighbors_col
+    def _score_num_neighbor(self, neighbors_row: int, neighbors_col: int) -> int:
+        return self.base_wall_fill_points + neighbors_row + neighbors_col
 
-    @classmethod
-    def _score_filled_row(cls, neighbors_row: int, size: int) -> int:
+    def _score_filled_row(self, neighbors_row: int, size: int) -> int:
         if neighbors_row == size - 1:
-            return cls.FILLED_ROW_SCORE
+            return self.filled_row_points
 
-    @classmethod
-    def _score_filled_col(cls, neighbors_row: int, size: int) -> int:
+    def _score_filled_col(self, neighbors_row: int, size: int) -> int:
         if neighbors_row == size - 1:
-            return cls.FILLED_COL_SCORE
+            return self.filled_col_points
 
-    @classmethod
-    def _score_filled_color(cls, tile_count: int, size: int) -> int:
+    def _score_filled_color(self, tile_count: int, size: int) -> int:
         if tile_count == size - 1:
-            return cls.FILLED_COLOR_SCORE
+            return self.filled_color_points
