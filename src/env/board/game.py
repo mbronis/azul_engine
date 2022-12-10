a4786d1701b7b5ed7221bdaed9e931f8068540f1
mbronis <michal.bronis@gmail.com>
2022-12-10 19:46:34 +0100

    Add AzulGame.reset

--- src/env/board/game.py
+++ src/env/board/game.py
@@ -1,39 +1,54 @@
+import random
+
 from src.env.board.rules import AzulRules
 from src.env.tiles import MultiTileLine, Tile, SingleTileLine
 from src.env.board.board import Board
 from src.env.walls import get_wall
 from src.env.states.game_state import GameState
+from src.env.scoring import Scorer
 
 
 class AzulGame:
     def __init__(self, rules: AzulRules) -> None:
+        # TODO: inject dependency
+        self.scorer = Scorer(rules)
+
+        self.terminated: bool = None
+        self.factory_size: int = rules.factory_size
+
         wall = get_wall(rules.wall_type)
         self.boards = [Board(wall)] * rules.num_players
-        self.tiles_bag = self._init_tiles_bag(tiles_count=rules.tiles_count)
         self.factories = [MultiTileLine()] * rules.num_factories
         self.mid_factory = MultiTileLine()
         self.discarded = MultiTileLine()
-
-        self.factory_size: int = rules.factory_size
-        self.terminated: bool = None
-
-    def _init_tiles_bag(self, tiles_count: int) -> MultiTileLine:
-        tiles = MultiTileLine()
+        self.tiles_bag = MultiTileLine()
         for tile in Tile:
-            l = SingleTileLine(tile=tile, size=tiles_count)
-            tiles.extend(l)
-        return tiles
+            l = SingleTileLine(tile=tile, size=rules.tiles_count, filled=0)
+            self.tiles_bag.extend(l)
 
-    def reset(self, seed: int = 1234) -> None:
+    def reset(self, seed: int = 1234) -> GameState:
+        self.terminated = False
+        self._set_random(seed)
+
         for board in self.boards:
             board.reset()
-        self.tiles_bag.fill_max()
         for factory in self.factories:
             factory.reset()
         self.mid_factory.reset()
         self.discarded.reset()
 
-        self.terminated = False
+        self.tiles_bag.fill_max()
+        self.fill_factories()
+
+        return self.get_state()
+
+    def fill_factories(self):
+        for factory in self.factories:
+            tiles = self.tiles_bag.get_random(n=self.factory_size)
+            factory.merge(tiles)
+
+    def _set_random(self, seed: int) -> None:
+        random.seed(seed)
 
     def get_state(self) -> GameState:
         # TODO: implement
