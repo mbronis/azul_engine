6f7deeeb54929e0b03bbf5cecbdf9873f74876ff
mbronis <michal.bronis@gmail.com>
2022-12-11 19:36:53 +0100

    Add Window.add_title

--- src/gui/cli.py
+++ src/gui/cli.py
@@ -9,11 +9,13 @@ class Window:
     lines: List[List[str]]
 
     @classmethod
-    def from_string(cls, lines: List[str], with_boarder: bool = False) -> Window:
+    def from_string(cls, lines: List[str], with_boarder: bool = False, title: str = None) -> Window:
         lines = [lines] if isinstance(lines, str) else lines
         w = Window(lines=[[*l] for l in lines])
         if with_boarder:
             w.add_boarder()
+        if title:
+            w.add_title(title)
         return w
 
     @property
@@ -22,12 +24,6 @@ class Window:
 
     def __getitem__(self, index: int) -> List[str]:
         return self.lines[index]
-
-    def add(self, w: Window, offset: Tuple[int, int]):
-        ox, oy = offset
-        for x, row in enumerate(w.lines):
-            for y, char in enumerate(row):
-                self.lines[x + ox][y + oy] = char
 
     def add_boarder(self):
         def get_char(pos: Tuple[int, int]) -> str:
@@ -53,9 +49,21 @@ class Window:
         boarder_lines = [[*l] for l in boarder_lines]
         self.lines = boarder_lines
 
+    def add(self, w: Window, offset: Tuple[int, int]):
+        ox, oy = offset
+        for x, row in enumerate(w.lines):
+            for y, char in enumerate(row):
+                self.lines[x + ox][y + oy] = char
+
+    def add_title(self, title: str):
+        _, y = self.shape
+        title = title[: y - 4]
+        t = Window.from_string(title, with_boarder=False)
+        self.add(t, (0, 2))
+
 
 class CliGui:
-    def __init__(self, x: int = 20, y: int = 80, fill: str = ".") -> None:
+    def __init__(self, x: int = 22, y: int = 80, fill: str = " ") -> None:
         self.x, self.y = x, y
         self.fill = fill
         self.image: Window = None
@@ -67,8 +75,8 @@ class CliGui:
             row = []
             for _ in range(self.y):
                 row.append(self.fill)
-            image_string.append(row)
-        self.image = Window(image_string)
+            image_string.append("".join(row))
+        self.image = Window.from_string(image_string, with_boarder=True)
 
     def add(self, w: Window, offset: Tuple[int, int]):
         self.image.add(w, offset)
@@ -83,27 +91,3 @@ class CliGui:
     def show(self):
         os.system("cls||clear")
         print(self.render())
-
-
-class AzulCliGui(CliGui):
-    def draw_state(self, state: dict) -> None:
-        """Updates image with state data"""
-        title = self._draw_title()
-        factories = self._draw_factories(state)
-
-        self.make_canvas()
-        self.add(title, (0, 0))
-        self.add(factories, (2, 1))
-
-    def _draw_title(self) -> Window:
-        return Window.from_string("Azul Game")
-
-    def _draw_factories(self, state: dict) -> Window:
-        width = 10
-        lines = ["    uyrbs "]
-        for factory_name, factory_state in state["factories"].items():
-            state_string = "".join([str(v) for v in factory_state.values()])
-            factory_line = f"{factory_name}: {state_string}"
-            lines.append(factory_line)
-        lines = [l.ljust(width, " ") for l in lines]
-        return Window.from_string(lines, with_boarder=True)
