f20f5c4d0a66d7f51fff61878f58098dcf9f0999
mbronis <michal.bronis@gmail.com>
2022-12-11 21:49:20 +0100

    Add draw_wall to AzulCliGui

--- src/gui/azul_cli.py
+++ src/gui/azul_cli.py
@@ -17,7 +17,7 @@ class AzulCliGui(CliGui):
         self.add(title, (0, 2))
         self.add(factories, (2, 2))
         self.add(legend, (factories.shape[0] + 3, 2))
-        self.add(board, (2, factories.shape[1] + 5))
+        self.add(board, (2, factories.shape[1] + 10))
 
     def _draw_title(self) -> Window:
         return Window.from_string_lines("Azul Game")
@@ -46,9 +46,12 @@ class AzulCliGui(CliGui):
         return Window.from_string_lines(lines, with_boarder=True, title="Legend")
 
     def _draw_board(self, board_state: dict) -> Window:
-        canvas = Window.canvas(8, 18, title=f"Score {board_state['score']}")
         pattern_lines = self._draw_pattern_lines(board_state)
+        wall = self._draw_wall(board_state)
+
+        canvas = Window.canvas(8, 20, title=f"Score {board_state['score']}")
         canvas.add(pattern_lines, (2, 2))
+        canvas.add(wall, (2, pattern_lines.shape[1] + 5))
         return canvas
 
     def _draw_pattern_lines(self, board_state: dict) -> Window:
@@ -63,3 +66,14 @@ class AzulCliGui(CliGui):
         width = max(len(l) for l in lines)
         lines = [l.rjust(width, " ") for l in lines]
         return Window.from_string_lines(lines)
+
+    def _draw_wall(self, board_state: dict) -> Window:
+        filled_lines = board_state["wall"]["filled"]
+        wall_lines = board_state["wall"]["expected"]
+        for x, row in enumerate(wall_lines):
+            for y, _ in enumerate(row):
+                if filled_lines[x][y] != ".":
+                    wall_lines[x][y] = filled_lines[x][y]
+
+        wall_lines = [" ".join(l) for l in wall_lines]
+        return Window.from_string_lines(wall_lines, with_boarder=False)
