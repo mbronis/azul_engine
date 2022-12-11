0c0e8f74b05b137d4e7e0411a942d922808c2ffe
mbronis <michal.bronis@gmail.com>
2022-12-11 22:46:13 +0100

    Add multiple boards to AzulCliGui

--- src/gui/azul_cli.py
+++ src/gui/azul_cli.py
@@ -3,7 +3,7 @@ from tests.tiles.test_single_tile_line import line
 
 
 class AzulCliGui(CliGui):
-    def __init__(self, x: int = 24, y: int = 80) -> None:
+    def __init__(self, x: int = 23, y: int = 70) -> None:
         super().__init__(x, y)
 
     def draw_state(self, state: dict) -> None:
@@ -11,13 +11,17 @@ class AzulCliGui(CliGui):
         title = self._draw_title()
         factories = self._draw_factories(state)
         legend = self._draw_legend()
-        board = self._draw_board(state["boards"]["board_1"])
+        boards = [self._draw_board(b) for b in state["boards"].values()]
+        boards *= 2
 
         self.reset_canvas()
         self.add(title, (0, 2))
         self.add(factories, (2, 2))
         self.add(legend, (factories.shape[0] + 3, 2))
-        self.add(board, (2, factories.shape[1] + 10))
+        for i, board in enumerate(boards):
+            x_offset = 2 + (1 + board.shape[0]) * (i > 1)
+            y_offset = 4 + factories.shape[1] + 2 + (2 + board.shape[1]) * (i % 2 == 1)
+            self.add(board, (x_offset, y_offset))
 
     def _draw_title(self) -> Window:
         return Window.from_string_lines("Azul Game")
@@ -46,15 +50,20 @@ class AzulCliGui(CliGui):
         return Window.from_string_lines(lines, with_boarder=True, title="Legend")
 
     def _draw_board(self, board_state: dict) -> Window:
+        score = self._draw_score(board_state)
         pattern_lines = self._draw_pattern_lines(board_state)
         wall = self._draw_wall(board_state)
         floor = self._draw_floor(board_state)
 
-        canvas = Window.canvas(8, 20, title=f"Score {board_state['score']}")
+        canvas = Window.canvas(8, 20, title=f"Name")
+        canvas.add(score, (1, 2))
         canvas.add(pattern_lines, (2, 2))
         canvas.add(wall, (2, pattern_lines.shape[1] + 5))
         canvas.add(floor, (wall.shape[0] + 3, 2))
         return canvas
+
+    def _draw_score(self, board_state: dict) -> Window:
+        return Window.from_string_lines(f"score: {board_state['score']}")
 
     def _draw_pattern_lines(self, board_state: dict) -> Window:
         def parse_pattern_line(pattern_line: dict) -> str:
