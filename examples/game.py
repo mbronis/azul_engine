35c9c614445e6d82c0943c49ae55396cd6c38312
mbronis <michal.bronis@gmail.com>
2022-12-15 00:14:24 +0100

    Update examples

--- examples/game.py
+++ examples/game.py
@@ -3,6 +3,7 @@ from src.gui.text import print_state
 from src.env.tiles import Tile
 from src.env.board.rules import get_rules
 from src.env.board.game import AzulGame
+from src.env.scoring import Scorer
 from src.env.messages import get_messages
 
 
@@ -10,8 +11,9 @@ def azul_game():
     num_players = 2
     wall_type = "fixed"
     rules = get_rules("standard")
+    scorer = Scorer(rules)
     messages = get_messages("default")
-    game = AzulGame(num_players=num_players, wall_type=wall_type, rules=rules, messages=messages)
+    game = AzulGame(num_players=num_players, wall_type=wall_type, rules=rules, scorer=scorer, messages=messages)
     state = game.reset(seed=1)
     return game, state
 
--- examples/human_input.py
+++ examples/human_input.py
@@ -3,19 +3,22 @@ from typing import Tuple
 from src.env.tiles import Tile
 from src.env.board.rules import get_rules
 from src.env.board.game import AzulGame
+from src.env.scoring import Scorer
 from src.gui.azul_cli import AzulCliGui
 from src.env.messages import get_messages
 
 
-NUM_PLAYERS = 1
+NUM_PLAYERS = 2
+SEED = 1
 
 
 def init_game() -> Tuple[AzulGame, dict]:
     wall_type = "fixed"
     rules = get_rules("standard")
+    scorer = Scorer(rules)
     messages = get_messages("default")
-    game = AzulGame(num_players=NUM_PLAYERS, wall_type=wall_type, rules=rules, messages=messages)
-    state = game.reset(seed=None)
+    game = AzulGame(num_players=NUM_PLAYERS, wall_type=wall_type, rules=rules, scorer=scorer, messages=messages)
+    state = game.reset(seed=SEED)
     return game, state
 
 
@@ -37,13 +40,13 @@ if __name__ == "__main__":
     game, state = init_game()
     gui = AzulCliGui(*AzulCliGui.players_to_canvas_shape(NUM_PLAYERS))
 
-    gui.draw_state(state, "Welcome to Azul Engine :D")
+    gui.draw_state(state, game.messages.welcome_message)
     gui.show()
     while True:
         prompt_factory, prompt_tile, prompt_row = input_prompt(state)
         factory_no = int(input(prompt_factory))
         tile = Tile(input(prompt_tile).upper())
-        row = input(prompt_factory)
+        row = input(prompt_row)
         row = None if not row else int(row)
         # factory_no, tile, row = int(inputs[0]), Tile(inputs[1].upper()), None if len(inputs) == 2 else int(inputs[2])
         action = {"factory_no": factory_no, "tile": tile, "row": row, "board_no": 0}
